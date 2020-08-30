7c69574092332acf41feda4b2d1391d50923d679
AustinFRG <austin.cartee@yahoo.com>
2020-08-30 19:52:08 -0400

    Fixed new help config

--- src/main/java/me/austinfrg/basicinfo/Commands/BasicInfoCommand.java
+++ src/main/java/me/austinfrg/basicinfo/Commands/BasicInfoCommand.java
@@ -34,58 +34,80 @@ public class BasicInfoCommand implements CommandExecutor {
             }
             if (getMinorVersion() == 7) {
                 player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 8) {
                 player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 9) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 10) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 11) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 12) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 13) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 14) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 15) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             if (getMinorVersion() == 16) {
                 player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+                for(String entry : plugin.getConfig().getStringList("helpcommand")){
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+                }
                 return true;
             }
             sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cBasicInfo is not currently compatible with your server version, please contact the plugin developer (AustinFRG) if you are using a version 1.7-1.16 and you see this message."));
             return true;
         }
-        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', String.valueOf(plugin.getConfig().getStringList("helpcommand"))));
+        for(String entry : plugin.getConfig().getStringList("helpcommand")){
+            sender.sendMessage(ChatColor.translateAlternateColorCodes('&', entry));
+        }
         return true;
     }
 }
