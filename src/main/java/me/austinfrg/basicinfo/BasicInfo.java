66adf621758bf1b6f23d42794aaf7f970cc85907
AustinFRG <austin.cartee@yahoo.com>
2020-09-02 12:44:39 -0400

    Fixed GUI

--- src/main/java/me/austinfrg/basicinfo/BasicInfo.java
+++ src/main/java/me/austinfrg/basicinfo/BasicInfo.java
@@ -1,15 +1,19 @@
 package me.austinfrg.basicinfo;
 
 import me.austinfrg.basicinfo.Commands.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public class BasicInfo extends JavaPlugin
-{
+public class BasicInfo extends JavaPlugin implements Listener {
 
     public void onEnable() {
         this.saveDefaultConfig();
+        Objects.requireNonNull(this.getCommand("info")).setExecutor(new InfoGUICommand(this));
         Objects.requireNonNull(this.getCommand("appeal")).setExecutor(new AppealCommand(this));
         Objects.requireNonNull(this.getCommand("website")).setExecutor(new WebsiteCommand(this));
         Objects.requireNonNull(this.getCommand("forums")).setExecutor(new ForumsCommand(this));
@@ -19,10 +23,23 @@ public class BasicInfo extends JavaPlugin
         Objects.requireNonNull(this.getCommand("appeal")).setExecutor(new AppealCommand(this));
         Objects.requireNonNull(this.getCommand("basicinfo")).setExecutor(new BasicInfoCommand(this));
         Objects.requireNonNull(this.getCommand("basicreload")).setExecutor(new BasicReloadCommand(this));
+        this.getServer().getPluginManager().registerEvents(this, this);
         int pluginId = 8180;
         new Metrics(this, pluginId);
     }
 
     public void onDisable() {
+    }
+
+    @EventHandler
+    public void onInventoryClickEvent(InventoryClickEvent event) {
+        Inventory inv = event.getClickedInventory();
+        if (inv == null) {
+            return;
+        }
+        String name = Objects.requireNonNull(inv).getType().name();
+        if (Objects.equals(name, this.getConfig().getString("guiname"))) {
+            event.setCancelled(true);
+        }
     }
 }
