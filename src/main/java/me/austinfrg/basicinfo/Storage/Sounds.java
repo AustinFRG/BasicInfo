8768cfffb1c89eb67a03848ab24e8cbf60ea93f3
AustinFRG <austin.cartee@yahoo.com>
2020-11-04 09:14:25 -0500

    Changed version check

--- src/main/java/me/austinfrg/basicinfo/Storage/Sounds.java
+++ src/main/java/me/austinfrg/basicinfo/Storage/Sounds.java
@@ -6,7 +6,7 @@ import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 public class Sounds {
-    public BasicInfo plugin;
+    public static BasicInfo plugin;
 
     public static String getVersion() {
         return Bukkit.getVersion().split("\\(MC: ")[1].split("\\)")[0];
@@ -18,36 +18,44 @@ public class Sounds {
     public static void soundEffects(Player player) {
         if (getMinorVersion() == 7) {
             player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 8) {
             player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 9) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 10) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 11) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 12) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 13) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 14) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 15) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
+            return;
         }
         if (getMinorVersion() == 16) {
             player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
+            return;
         }
-        else {
-            Messages.sendMSG(player, "&cBasicInfo is not currently compatible with your server version, please contact the plugin developer (AustinFRG) if you are using a version 1.7-1.16 and you see this message.");
-        }
+        Messages.sendMSG(player, "&cBasicInfo is not currently compatible with your server version, please contact the plugin developer (" + plugin.getDescription().getAuthors().toString() + ") if you'd like to use this plugin with your version.");
     }
 }
