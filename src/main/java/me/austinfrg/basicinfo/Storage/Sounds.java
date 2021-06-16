90a0a97818343654b072e9af049cd39e7ca98d74
AustinFRG <austin.cartee@yahoo.com>
2021-06-16 06:28:24 -0400

    Optimized code

--- src/main/java/me/austinfrg/basicinfo/Storage/Sounds.java
+++ src/main/java/me/austinfrg/basicinfo/Storage/Sounds.java
@@ -1,61 +1,28 @@
 package me.austinfrg.basicinfo.Storage;
 
+import com.cryptomorin.xseries.XSound;
 import me.austinfrg.basicinfo.BasicInfo;
-import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
 
 public class Sounds {
-    public static BasicInfo plugin;
-
-    public static String getVersion() {
-        return Bukkit.getVersion().split("\\(MC: ")[1].split("\\)")[0];
-    }
-    public static int getMinorVersion() {
-        return Integer.parseInt(getVersion().split("\\.")[1]);
-    }
+    public static BasicInfo plugin = JavaPlugin.getPlugin(BasicInfo.class);
 
     public static void soundEffects(Player player) {
-        if (getMinorVersion() == 7) {
-            player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
+        Sound sound;
+        try {
+            sound = XSound.valueOf(plugin.getConfig().getString("commandSound")).parseSound();
+            if (sound == null) {
+                plugin.getLogger().log(Level.WARNING, "Unable to find the sound set in config.");
+                return;
+            }
+        } catch (Exception ex) {
+            plugin.getLogger().log(Level.WARNING, "Unable to find the sound set in config.");
             return;
         }
-        if (getMinorVersion() == 8) {
-            player.playSound(player.getLocation(), Sound.valueOf("NOTE_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 9) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 10) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 11) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 12) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 13) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 14) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 15) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-            return;
-        }
-        if (getMinorVersion() == 16) {
-            player.playSound(player.getLocation(), Sound.valueOf("BLOCK_NOTE_BLOCK_PLING"), 1f, 1f);
-            return;
-        }
-        Messages.sendMSG(player, "&cBasicInfo is not currently compatible with your server version, please contact the plugin developer (" + plugin.getDescription().getAuthors().toString() + ") if you'd like to use this plugin with your version.");
+        player.playSound(player.getLocation(), sound, 1f, 1f);
     }
 }
