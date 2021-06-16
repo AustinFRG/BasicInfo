3a39f02e2017eb9c1d49cf91997ceca756293e51
AustinFRG <austin.cartee@yahoo.com>
2021-06-16 06:29:19 -0400

    Update

--- src/main/java/me/austinfrg/basicinfo/Storage/Messages.java
+++ src/main/java/me/austinfrg/basicinfo/Storage/Messages.java
@@ -3,9 +3,10 @@ package me.austinfrg.basicinfo.Storage;
 import me.austinfrg.basicinfo.BasicInfo;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class Messages {
-    public static BasicInfo plugin;
+    public static BasicInfo plugin = JavaPlugin.getPlugin(BasicInfo.class);
 
     public static void sendMSG(CommandSender player, String message) {
         player.sendMessage(ChatColor.translateAlternateColorCodes('&', message));
